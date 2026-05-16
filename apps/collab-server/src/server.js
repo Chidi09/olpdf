ae7ad361b07448e5b2293dfc3703c5c3b39554fd
chidi09 <chidiisking7@gmail.com>
2026-05-16 11:52:24 +0100

    fix: use y-leveldb persistence adapter

--- apps/collab-server/src/server.js
+++ apps/collab-server/src/server.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
 const http = require('http');
 const jwt = require('jsonwebtoken');
+const Y = require('yjs');
 const { WebSocketServer } = require('ws');
 const { setPersistence, setupWSConnection } = require('y-websocket/bin/utils');
-const { setupPersistence } = require('y-leveldb');
+const { LeveldbPersistence } = require('y-leveldb');
 
 const JWT_SECRET = process.env.JWT_SECRET || '';
 const server = http.createServer((_req, res) => res.end('Collab server'));
@@ -32,8 +33,15 @@ server.on('upgrade', (request, socket, head) => {
 wss.on('connection', setupWSConnection);
 
 const LEVELDB_PATH = process.env.LEVELDB_PATH || './data';
-const persistence = setupPersistence(LEVELDB_PATH);
-setPersistence(persistence);
+const persistence = new LeveldbPersistence(LEVELDB_PATH);
+setPersistence({
+  bindState: async (docName, ydoc) => {
+    const persistedYdoc = await persistence.getYDoc(docName);
+    Y.applyUpdate(ydoc, Y.encodeStateAsUpdate(persistedYdoc));
+    ydoc.on('update', (update) => persistence.storeUpdate(docName, update));
+  },
+  writeState: async () => {},
+});
 
 server.listen(process.env.PORT || 1234, () => {
   console.log(`Collab server on port ${process.env.PORT || 1234}`);
